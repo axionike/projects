c963dfb74262477f1275c3f430fa295b1b696f29
IHL <axionike@gmail.com>
2014-11-07 16:29:52 -0800

    added daily/2

---
+++ daily/2/easy2.c
@@ -0,0 +1,12 @@
+/* Reddit dailyprogrammer challenge
+** easy2.c
+** Life expectancy calculator
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static char currentage[2048];
+
+int main(int argc, char** argv) {
+	
